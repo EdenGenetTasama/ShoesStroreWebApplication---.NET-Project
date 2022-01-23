4902bcb705787b734daa6c044acc2be001ef42ae
EdenGenetTasama <edentasama19@gmail.com>
2022-01-23 18:46:40 +0200

    add shirts view

---
+++ Controllers/ShirtsController.cs
@@ -0,0 +1,58 @@
+﻿using ShoesStroreWebApplication__.NET_Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShoesStroreWebApplication__.NET_Project.Controllers
+{
+    public class ShirtsController : Controller
+    {
+        ShirtsContextDataContext dbCloting = new ShirtsContextDataContext();
+     
+
+        // GET: Shirts
+        public ActionResult ShirtsPage()
+        {
+            var listOfShirts = dbCloting.clothings.GroupBy(item => item.clothingType == "Shirt");
+            foreach (var shirt in listOfShirts)
+            {
+                ViewBag.AllShirts = shirt;
+
+            }
+        
+            return View();
+        }
+
+        // GET: Shirts/Details/5
+        public ActionResult MangerTableShirts()
+        {
+            var listOfShirts = dbCloting.clothings.GroupBy(item => item.clothingType == "Shirt");
+            foreach (var shirt in listOfShirts)
+            {
+                ViewBag.AllShirts = shirt;
+
+            }
+
+            return View();
+        }
+
+        // GET: Shirts/Create
+        public ActionResult GetOnlyLongShirts()
+        {
+            var onlyLongShorts = dbCloting.clothings.GroupBy(item => item.clothingType == "Shirt" & item.isClothingShort == false);
+            foreach (var shirt in onlyLongShorts)
+            {
+                ViewBag.onlyLongShirt = shirt;
+
+            }
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
