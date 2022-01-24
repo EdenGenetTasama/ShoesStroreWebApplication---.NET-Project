7c0a062ae4e41c774dc69ebd9d6f05726778c3c9
EdenGenetTasama <edentasama19@gmail.com>
2022-01-24 21:51:36 +0200

    add sport Equipment Pages , controllers and wiev

--- Controllers/PantsController.cs
+++ Controllers/PantsController.cs
@@ -22,42 +22,55 @@ namespace ShoesStroreWebApplication__.NET_Project.Controllers
         public ActionResult MangerTable()
         {
             List<clothing> listOfPantsToTable = dbCloting.clothings.Where(item => item.clothingType == "Pants").ToList();
-            if (listOfPantsToTable.Count != null)
-            {
+          
                 ViewBag.AllPantsToTable = listOfPantsToTable;
                 return View();
-            }
-            return View("NOT FOUND ITEM");
+
+
         }
 
         public ActionResult OnlyLongPants()
         {
             List<clothing> onlyLongPants = dbCloting.clothings.Where(item => item.clothingType == "Pants" & item.isClothingShort == false).ToList();
+          
             ViewBag.onlyLongPants = onlyLongPants;
            
             return View();
+      
 
         }
         public ActionResult OnlyShortsPants()
         {
             List<clothing> onlyPantsShirts = dbCloting.clothings.Where(item => item.clothingType == "Pants" & item.isClothingShort == true).ToList();
+
             ViewBag.onlyShortPants= onlyPantsShirts;
             return View();
+           
+            //return Content("<script language='javascript' type='text/javascript'>alert('Sorry item not found, Going to all pants');</script>");
+            //return RedirectToAction("ShowPantsall") ;
+
 
         }
 
         public ActionResult GetOnlyDryFitPants()
+
         {
             List<clothing> onlyDryFitPants = dbCloting.clothings.Where(item => item.clothingType == "Pants" & item.isClothingDrifit == true).ToList();
-            ViewBag.onlyDryFitPants = onlyDryFitPants;
+          
+             ViewBag.onlyDryFitPants = onlyDryFitPants;
             return View();
+        
         }
 
         public ActionResult SortByPricePants()
         {
             List<clothing> listOfShirts = dbCloting.clothings.Where(item => item.clothingType == "Pants").OrderBy(clothe => clothe.clothingPrice).ToList();
+           
             ViewBag.sortByPrice = listOfShirts;
             return View();
+
+            //return RedirectToAction("ShowPantsall");
+
         }
 
 
---
+++ Controllers/SportEquipmentController.cs
@@ -0,0 +1,49 @@
+﻿using ShoesStroreWebApplication__.NET_Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShoesStroreWebApplication__.NET_Project.Controllers
+{
+    public class SportEquipmentController : Controller
+    {
+        SportEquipmentContextDataContext sportDb = new SportEquipmentContextDataContext();
+        // GET: SportEquipment
+        public ActionResult EquipmentPage()
+        {
+            ViewBag.allEquipment = sportDb.sportEquipments.ToList();
+            return View();
+        }
+
+        public ActionResult MangerMode()
+        {
+            ViewBag.MangerMode = sportDb.sportEquipments.ToList();
+            return View();
+        }
+
+        public ActionResult OnlyBaskBallEquipment()
+        {
+            List<sportEquipment> listOfBaskball = sportDb.sportEquipments.Where(item=> item.sportBranch == "Basketball").ToList();
+            ViewBag.onlyBaskball = listOfBaskball;
+            return View(); 
+
+        }
+
+        public ActionResult OnlyFootballEquipment()
+        {
+            List<sportEquipment> listOfFootball = sportDb.sportEquipments.Where(item => item.sportBranch == "Football").ToList();
+            ViewBag.onlyFootball = listOfFootball;
+            return View();
+
+        }
+        public ActionResult SortEquipment()
+        {
+            List<sportEquipment> listToSort = sportDb.sportEquipments.OrderBy(item=> item.productPrice).ToList();
+            ViewBag.toSort = listToSort;
+            return View();
+
+        }
+    }
+}
