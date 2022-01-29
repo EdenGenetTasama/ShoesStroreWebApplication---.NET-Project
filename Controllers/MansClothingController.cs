4a35043b4a4a028879398322590f9c52b381720b
EdenGenetTasama <edentasama19@gmail.com>
2022-01-29 14:11:53 +0200

    add style

---
+++ Controllers/MansClothingController.cs
@@ -0,0 +1,35 @@
+﻿using ShoesStroreWebApplication__.NET_Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShoesStroreWebApplication__.NET_Project.Controllers
+{
+    public class MansClothingController : Controller
+    {
+        ShirtsContextDataContext clothingDB = new ShirtsContextDataContext();
+        // GET: MansClothing
+        public ActionResult onlyMansClothes()
+        {
+            List<clothing> onlyMansCloth = clothingDB.clothings.Where(item => item.clothingGender == "Man").ToList();
+            ViewBag.MansCloth= onlyMansCloth;
+            return View();
+        }
+
+        public ActionResult OnlyWomenClothes()
+        {
+            List<clothing> onlyMansCloth = clothingDB.clothings.Where(item => item.clothingGender == "Female").ToList();
+            ViewBag.WomenCloth= onlyMansCloth;
+            return View();
+        }
+
+        public ActionResult OnlyKidesClothes()
+        {
+            List<clothing> onlyKidsCloth = clothingDB.clothings.Where(item => item.clothingGender == "Kids").ToList();
+            ViewBag.KidsCloth= onlyKidsCloth;
+            return View();
+        }
+    }
+}
